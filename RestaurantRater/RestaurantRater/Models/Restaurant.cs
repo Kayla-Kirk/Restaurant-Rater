1ddd878fab8bc27907dcf3f7063b445ea8126fdf
Kayla-Kirk <Therainbowbitme@gmail.com>
2021-06-24 18:30:17 -0400

    first push

---
+++ RestaurantRater/RestaurantRater/Models/Restaurant.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RestaurantRater.Models
+{
+    public class Restaurant
+    {
+        public int RestaurtantID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Rating { get; set; }
+    }
+
+    public class RestaurantDbContext : DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
